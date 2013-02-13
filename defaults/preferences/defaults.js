51b99cd80dd9e7531d61f17d641e7098ca3593bc
Duncan Sample <duncan.sample+github@gmail.com>
2013-02-13 22:47:05 +0000

    Update defaults/preferences/defaults.js

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,8 +1,8 @@
-<!-- Either "QR" for QRCode or "DM" for DataMatrix -->
+// Either "QR" for QRCode or "DM" for DataMatrix
 pref("mobilebarcode.type", "QR");
 
-<!-- Choose between S, M, L, XL, XXL -->
+// Choose between S, M, L, XL, XXL
 pref("mobilebarcode.size", "M");
 
-<!-- Choose between nokia, google and kaywa -->
+// Choose between nokia, google and kaywa
 pref("mobilebarcode.provider", "google");
